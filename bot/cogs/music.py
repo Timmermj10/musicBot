1e8c99ac5efeba0b0468b455a49ad3253bfb5915
Jacob Timmerman <timmermj@umich.edu>
2024-09-29 19:35:36 -0400

    Working Music Bot, next up is logging and transfering it to run on the RaspberryPi

--- bot/cogs/music.py
+++ bot/cogs/music.py
@@ -1,160 +1,166 @@
 import discord
 import config
-from discord.ext import commands
+import asyncio
 import yt_dlp
+from discord.ext import commands
 
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        # All music related data structures
-        self.isPlaying = False
-
-        # 2D array [song, channel]
-        self.queue = []
-        self.yt_dl_options = {"format": "bestaudio/best"}
+        self.queues = {}
+        self.voice_clients = {}
+        self.yt_dl_options = {"format": "bestaudio/best", "noplaylist": True, "quiet": True} # "verbose": True
         self.ytdl = yt_dlp.YoutubeDL(self.yt_dl_options)
 
         self.ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn -filter:a "volume=0.25"'}
-        
-        self.vc = None
+
+    async def play_next(self, ctx):
+        if self.queues[ctx.guild.id] != []:
+            player, title = self.queues[ctx.guild.id].pop(0)
+            
+            await ctx.send(f'Playing {title}')
+            await self.play_song(ctx, player)
+    
+    async def play_song(self, ctx, player):
+        self.voice_clients[ctx.guild.id].play(player, after=lambda e: asyncio.run(self.play_next(ctx)))
+    
+    async def find251(self, formats):
+        for format in formats:
+            # print(format['format_id'])
+            if format['format_id'] == '251':
+                return format
+
+    async def search_yt(self, ctx, query):
+        await ctx.send('Searching for song...')
+        loop = asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: self.ytdl.extract_info(f"ytsearch:{query} + official", download=False)['entries'][0])
+        format_251 = await self.find251(data['formats'])
+
+        song = format_251['url']
+
+        player = discord.FFmpegOpusAudio(song, **self.ffmpeg_options)
+
+        return player, data['title']
+
+    async def connect_voice(self, ctx):
+        if ctx.author.voice is None:
+            await ctx.send('You are not in a voice channel.')
+            return False
+        if not self.voice_clients or ctx.guild.id not in self.voice_clients:
+            voice_client = await ctx.author.voice.channel.connect()
+            self.voice_clients[voice_client.guild.id] = voice_client
+            return True
+        return True
 
     # Listen for the on_ready event
     @commands.Cog.listener()
     async def on_ready(self):
         print('Music cog loaded.')
-        await self.check_voice_clients()
-
-    async def check_voice_clients(self):
-        for guild in self.bot.guilds:
-            if guild.voice_client:
-                self.vc = guild.voice_client
-                print(f'Bot is connected to a voice channel in guild: {guild.name}')
-                break
-        else:
-            print('Bot is not connected to any voice channels.')
-
-    # Search the song on youtube
-    def search_yt(self, item):
-        with yt_dlp.YoutubeDL(self.yt_dl_options) as ydl:
-            try:
-                info = ydl.extract_info(f"ytsearch:{item}", download=False)['entries'][0]
-            except Exception:
-                return False
-        return {'source': info['formats'][0]['url'], 'title': info['title']}
-    
-    # Play next song
-    def play_next(self):
-        # If we have a song in the queue
-        if len(self.queue) > 0:
-            # Mark the bot as playing
-            self.isPlaying = True
-
-            # Get the url of the song
-            url = self.queue[0][0]['source']
-
-            # Pop the song from the queue
-            self.queue.pop(0)
-
-            # Play the song
-            self.vc.play(discord.FFmpegPCMAudio(url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
-        else:
-            self.isPlaying = False
-
-    # Infinite loop checker
-    async def check_queue(self):
-        print('Checking queue')
-        if len(self.queue) > 0:
-            print('Queue is not empty')
-            # Mark the bot as playing
-            self.isPlaying = True
-
-            # Get the url of the song
-            url = self.queue[0][0]['source']
-
-            print(url)
-            # Try to connect to the voice channel if the bot is not already connected
-            if not self.vc or not self.vc.is_connected():
-                print('Connecting to voice channel')
-                self.vc = await self.queue[0][1].connect()
-            else:
-                print('Moving to voice channel')
-                self.vc = await self.vc.move_to(self.queue[0][1])
-
-            print('Queue', self.queue)
-            #  Pop the song from the queue
-            self.queue.pop(0)
-
-            # Play the song
-            print('Playing song in', self.vc)
-            self.vc.play(discord.FFmpegOpusAudio(url, **self.ffmpeg_options), after=lambda e: self.play_next())
-        else:
-            self.isPlaying = False
 
     @commands.command(name='play', help='Plays a selected song from Youtube')
-    async def p(self, ctx, *args):
-        print('I am actually in this file!')
-        query = ' '.join(args)
-
-        voice_channel = discord.utils.get(ctx.guild.voice_channels, name='general')
-        if not voice_channel:
-            await ctx.send('You need to be in a voice channel.')
-        else:
-            song = self.search_yt(query)
-            if type(song) == type(True):
-                await ctx.send("Could not download the song. Incorrect format try another keyword. This could be due to playlist or a livestream format.")
+    async def play(self, ctx, *args):
+        # Check if the user passed in any arguments
+        if args == ():
+            if ctx.guild.id in self.queues and len(self.queues[ctx.guild.id]) > 0:
+                # Connect the bot to the voice channel
+                connection = await self.connect_voice(ctx)
+                if connection:
+                    await self.play_next(ctx)
             else:
-                await ctx.send(f'Added {song["title"]} to the queue')
-                self.queue.append([song, voice_channel])
+                await ctx.send('Please provide a song to play.')
+            return
 
-                if self.isPlaying == False:
-                    await self.check_queue()
+        try:
+            # Connect the bot to the voice channel
+            connection = await self.connect_voice(ctx)
+            if not connection:
+                return
+            player, title = await self.search_yt(ctx, ' '.join(args))
 
-    @commands.command(name='queue', help='Displays the current music queue')
-    async def q(self, ctx):
-        if len(self.queue) == 0:
-            await ctx.send('No music in queue')
-        else:
-            queue_list = ''
-            for i in range(len(self.queue)):
-                queue_list += self.queue[i][0]['title'] + '\n'
-            await ctx.send(queue_list)
-
-    @commands.command(name='skip', help='Skips the current song')
-    async def skip(self, ctx):
-        if self.vc:
-            # Stop the current song
-            self.vc.stop()
-
-            # Try and play the next song in the queue
-            await self.check_queue()
-
-    @commands.command(name='leave', help='Leaves the voice channel')
-    async def leave(self, ctx):
-        print('Leaving voice channel', self.vc)
-        if self.vc:
-            # Disconnect from the voice channel
-            await self.vc.disconnect()
-
-            # Update the vc variable
-            self.vc = None
+            await ctx.send(f'Playing {title}')
+        
+            self.voice_clients[ctx.guild.id].play(player, after=lambda e: asyncio.run(self.play_next(ctx)))
+        except Exception as e:
+            print(e)
 
     @commands.command(name='pause', help='Pauses the current song')
     async def pause(self, ctx):
-        voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
-        if voice.is_playing():
-            voice.pause()
-        else:
-            await ctx.send("No audio is playing right now.")
+        if ctx.guild.id not in self.voice_clients or not self.voice_clients[ctx.guild.id].is_playing():
+            await ctx.send('Not currently playing anything.')
+            return
+        try:
+            self.voice_clients[ctx.guild.id].pause()
+        except Exception as e:
+            print(e)
 
     @commands.command(name='resume', help='Resumes the current song')
     async def resume(self, ctx):
-        voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
-        if voice.is_paused():
-            voice.resume()
-        else:
-            await ctx.send("The audio is not paused.")
+        if ctx.guild.id not in self.voice_clients or not self.voice_clients[ctx.guild.id].is_paused():
+            await ctx.send('Not currently paused.')
+            return
+        try:
+            self.voice_clients[ctx.guild.id].resume()
+        except Exception as e:
+            print(e)
 
+    @commands.command(name='stop', help='Stops the current song and leaves the voice channel')
+    async def stop(self, ctx):
+        try:
+            self.voice_clients[ctx.guild.id].stop()
+            await self.voice_clients[ctx.guild.id].disconnect()
+            del self.voice_clients[ctx.guild.id]
+        except Exception as e:
+            print(e)
+
+    @commands.command(name='queue', help='Displays the current queue')
+    async def queue(self, ctx, *args):
+        if ctx.guild.id not in self.queues:
+            self.queues[ctx.guild.id] = []
+        
+        # Get the player and title of the song and add it to the queue
+        player, title = await self.search_yt(ctx, ' '.join(args))
+        self.queues[ctx.guild.id].append((player, title))
+        await ctx.send(f'Added {title} to the queue.')
+
+    @commands.command(name='skip', help='Skips the current song')
+    async def skip(self, ctx):
+        # If there is no voice client associate or we are not playing anything
+        if ctx.guild.id not in self.voice_clients or not self.voice_clients[ctx.guild.id].is_playing():
+            await ctx.send('Not currently playing anything.')
+            return
+        
+        # If there is a voice client associate, we are playing something, and there are no songs in the queue
+        if self.voice_clients[ctx.guild.id].is_playing() and (ctx.guild.id not in self.queues or len(self.queues[ctx.guild.id]) == 0):
+            self.voice_clients[ctx.guild.id].stop()
+            await ctx.send('No songs in queue.')
+            return
+
+        # If there is a voice client associate, we are playing something, and there are songs in the queue
+        if self.voice_clients[ctx.guild.id].is_playing():
+            self.voice_clients[ctx.guild.id].stop()
+            await self.play_next(ctx)
+
+    @commands.command(name='clear', help='Clears the current queue')
+    async def clear(self, ctx):
+        if ctx.guild.id in self.queues and len(self.queues[ctx.guild.id]) > 0:
+            self.queues[ctx.guild.id].clear()
+            await ctx.send('Queue cleared.')
+        else:
+            await ctx.send('Nothing in Queue.')
+
+    # Command to view the queue
+    @commands.command(name='view', help='View the current queue')
+    async def view(self, ctx):
+        if ctx.guild.id in self.queues and len(self.queues[ctx.guild.id]) > 0:
+            queue = ''
+            for i, (player, title) in enumerate(self.queues[ctx.guild.id]):
+                queue += f'{i + 1}. {title}\n'
+            await ctx.send(queue)
+        else:
+            await ctx.send('No songs in queue.')
+
+# Add the cog to the bot
 async def setup(bot):
     # Pass the GUILD_ID (Server ID) to the question cog to be used in the ctx.guild parameter)
-    await bot.add_cog(Music(bot), guilds=[discord.Object(id=config.GUILD_ID)])
+    await bot.add_cog(Music(bot))
